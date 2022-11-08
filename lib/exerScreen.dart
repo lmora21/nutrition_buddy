d00138c691735a90d9fd33a78082b2283c5c0ea2
ggonza55 <ggonza55@uic.edu>
2022-11-08 11:26:31 -0600

    Removed redundant file

--- lib/exerScreen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-// Screen content for when Exercise icon is pressed on bottom tab
-bool isChecked = false;
-Column get exerScreen => Column(
-      children: const <Widget>[
-      ],
-    );
