769f0e6435713f00f0e56b3197d80dbddc783f39
lmora21 <lmora21@uic.edu>
2022-11-24 13:41:41 -0600

    Added padding, fixed text that looks like hyperlinks, and fixed overlapping icons

--- lib/dietInfoScreen.dart
+++ lib/dietInfoScreen.dart
@@ -6,7 +6,7 @@ class DietInfoScreen extends StatelessWidget {
   //  Using int index for according Grocery item in GroceryList
   final int itemIndex;
 
-  DietInfoScreen(this.itemIndex, {Key? key}) : super(key: key);
+  const DietInfoScreen(this.itemIndex, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,67 +17,67 @@ class DietInfoScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Padding(padding: EdgeInsets.only(top: 10)),
-            Text(GroceryList.titles[itemIndex], style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline, fontSize: 40),),
-            Text("${GroceryList.calPerServ[itemIndex]} cal per serving of ${GroceryList.servings[itemIndex]}", style: TextStyle(fontSize: 20),),
-            Padding(padding: EdgeInsets.only(top: 10)),
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            Text(GroceryList.titles[itemIndex], style: const TextStyle(color: Colors.black, decoration: TextDecoration.underline, fontSize: 40),),
+            Text("${GroceryList.calPerServ[itemIndex]} cal per serving of ${GroceryList.servings[itemIndex]}", style: const TextStyle(fontSize: 20),),
+            const Padding(padding: EdgeInsets.only(top: 10)),
               //  Need 5 rows for each nutritional info value - fat, carbs, protein, etc.
               //  Fat
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text("Total Fat: ${GroceryList.fatPerServ[itemIndex]}g", style: TextStyle(fontSize: 40),),
+                  Text("Total Fat: ${GroceryList.fatPerServ[itemIndex]}g", style: const TextStyle(fontSize: 40),),
                 ],),
-              Divider(color: Colors.black, thickness: 0.5),
+              const Divider(color: Colors.black),
               // Carb
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Carbohydrates: ${GroceryList.carbPerServ[itemIndex]}g", style: TextStyle(fontSize: 40),),
+                Text("Carbohydrates: ${GroceryList.carbPerServ[itemIndex]}g", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
             //  Cholesterol
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Cholesterol: ${GroceryList.cholPerServ[itemIndex]}mg", style: TextStyle(fontSize: 40),),
+                Text("Cholesterol: ${GroceryList.cholPerServ[itemIndex]}mg", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
             //  Sodium
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Sodium: ${GroceryList.sodiumPerServ[itemIndex]}mg", style: TextStyle(fontSize: 40),),
+                Text("Sodium: ${GroceryList.sodiumPerServ[itemIndex]}mg", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
             // Protein
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Protein: ${GroceryList.proteinPerServ[itemIndex]}g", style: TextStyle(fontSize: 40),),
+                Text("Protein: ${GroceryList.proteinPerServ[itemIndex]}g", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
             //  Potassium
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Potassium: ${GroceryList.ptsmPerServ[itemIndex]}mg", style: TextStyle(fontSize: 40),),
+                Text("Potassium: ${GroceryList.ptsmPerServ[itemIndex]}mg", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
             //  Fiber
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Fiber: ${GroceryList.fiberPerServ[itemIndex]}g", style: TextStyle(fontSize: 40),),
+                Text("Fiber: ${GroceryList.fiberPerServ[itemIndex]}g", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
             //  Sugars
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Sugar: ${GroceryList.sugarsPerServ[itemIndex]}g", style: TextStyle(fontSize: 40),),
+                Text("Sugar: ${GroceryList.sugarsPerServ[itemIndex]}g", style: const TextStyle(fontSize: 40),),
               ],),
-            Divider(color: Colors.black, thickness: 0.5),
+            const Divider(color: Colors.black),
           ],
         ),
       ),
