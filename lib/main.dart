b7c537cf615fe17d7f11e2373015a665bb2b6315
ggonza55 <ggonza55@uic.edu>
2022-11-24 21:31:08 -0600

    Fixed checkbox synced bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cs422_gr4_nutribuddy/home_route_cal.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -92,13 +93,12 @@ class _MyHomePageState extends State<MyHomePage> {
   int _newNdx = 0;
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-
     Widget exerciseItem(
         {required String title,
         required String hint,
         required String desc,
-        required String img}) {
+        required String img,
+        required int index}) {
       return GestureDetector(
         child: Column(
           children: [
@@ -117,10 +117,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
                       child: Checkbox(
-                        value: isChecked[0],
+                        value: isChecked[index],
                         onChanged: (bool? value) {
                           setState(() {
-                            isChecked[0] = value!;
+                            isChecked[index] = value!;
                           });
                         },
                       ),
@@ -129,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-            const Divider(color: Colors.black)
+            indentedDivider(),
           ],
         ),
         onTap: () {
@@ -144,7 +144,7 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
 
-    Widget restItem({required int minutes}) {
+    Widget restItem({required int minutes, required int index}) {
       return Column(
         children: [
           Row(
@@ -158,11 +158,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
                   child: Checkbox(
-                    value: isChecked[1],
+                    value: isChecked[index],
                     onChanged: (bool? value) {
                       setState(
                         () {
-                          isChecked[1] = value!;
+                          isChecked[index] = value!;
                         },
                       );
                     },
@@ -171,7 +171,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-          const Divider(color: Colors.black),
+          indentedDivider(),
         ],
       );
     }
@@ -201,36 +201,44 @@ class _MyHomePageState extends State<MyHomePage> {
                       desc:
                           "1. Start by lying on your back with your knees bent. Sit ups work best if you do them on a soft surface, such as a mattress. Keep your knees bent at a 90-degree angle, with your feet flat on the floor.\n\n2. Put your fingertips on the back of your ears. Your elbows should be bent and pointing out at your sides. Cupping the back of your ears with your fingertips — as opposed to placing them on the back of your head — can help prevent you from pulling yourself up by your neck as you’re doing sit ups.\n\n3. Lift your torso up as close to your thighs as possible. Do this with a smooth, steady motion, keeping your feet flat on the floor. When you’re done lifting your torso, your lower back should be off the floor.\n\n4. Lower your torso down to the floor so you’re back in the starting position. Like you did when you lifted your torso up to your thighs, use a smooth and steady motion as you lower it down.",
                       img: "situps.gif",
+                      index: 0,
                     ),
-                    restItem(minutes: 2),
+                    restItem(minutes: 2, index: 1),
                     exerciseItem(
                       title: "Push Ups",
                       hint: "(15 reps)",
                       desc:
                           "1. Assume a face-down prone position on the floor. Keep your feet together. Your weight should be on your chest. Position hands palms-down on the floor, approximately shoulder width apart. They should be about next to your shoulders, with your elbows pointed towards your toes.\n\n2. Raise yourself using your arms. At this point, your weight should be supported by your hands and the balls of your feet. Make a straight line from your head to your heels, and contract your abdominals to keep your hips from sagging. This position is called a \"plank,\" which is used for other various exercises. This is the beginning and the end position of a single push up.",
                       img: "pushups.gif",
+                      index: 2,
                     ),
-                    restItem(minutes: 2),
+                    restItem(minutes: 2, index: 3),
                     exerciseItem(
-                        title: "Plank",
-                        hint: "(1 minute)",
-                        desc:
-                            "To do a traditional plank, lay on your stomach on the floor with your arms bent at the elbow so your hands are under your shoulders. Raise up slowly so that your arms are extended and you're on your toes (or your knees, if you're a beginner) with your legs and back straight. Hold the position for 5 to 10 seconds and gradually work up to 3 sets of 60 seconds each.",
-                        img: "plank.jpg"),
-                    restItem(minutes: 1),
+                      title: "Plank",
+                      hint: "(1 minute)",
+                      desc:
+                          "To do a traditional plank, lay on your stomach on the floor with your arms bent at the elbow so your hands are under your shoulders. Raise up slowly so that your arms are extended and you're on your toes (or your knees, if you're a beginner) with your legs and back straight. Hold the position for 5 to 10 seconds and gradually work up to 3 sets of 60 seconds each.",
+                      img: "plank.jpg",
+                      index: 4,
+                    ),
+                    restItem(minutes: 1, index: 5),
                     exerciseItem(
-                        title: "Pull Ups",
-                        hint: "(10 reps)",
-                        desc:
-                            "Grip a pullup bar with your palms facing whichever direction you prefer. In general, having your palms facing towards you is most efficient. When you pull yourself up with your hands facing this way, you give your biceps and lats a better workout. Pulling yourself up with your palms out is considered the most difficult way to pull up your bodyweight but also gives deltoids and triceps a good workout. Start with your arms fully extended. \n\n2. Pull your bodyweight up until your chin is just barely above the bar. You may have to strain, but keep pulling until you've lifted yourself up using your back and biceps.\n\n3. Lower yourself until your arms are fully extended. Lower yourself in a controlled way to work the muscles harder and prep yourself for the next pull.",
-                        img: "pullups.gif"),
-                    restItem(minutes: 2),
+                      title: "Pull Ups",
+                      hint: "(10 reps)",
+                      desc:
+                          "Grip a pullup bar with your palms facing whichever direction you prefer. In general, having your palms facing towards you is most efficient. When you pull yourself up with your hands facing this way, you give your biceps and lats a better workout. Pulling yourself up with your palms out is considered the most difficult way to pull up your bodyweight but also gives deltoids and triceps a good workout. Start with your arms fully extended. \n\n2. Pull your bodyweight up until your chin is just barely above the bar. You may have to strain, but keep pulling until you've lifted yourself up using your back and biceps.\n\n3. Lower yourself until your arms are fully extended. Lower yourself in a controlled way to work the muscles harder and prep yourself for the next pull.",
+                      img: "pullups.gif",
+                      index: 6,
+                    ),
+                    restItem(minutes: 2, index: 7),
                     exerciseItem(
-                        title: "Burpees",
-                        hint: "(20 reps)",
-                        desc:
-                            "1. Stand with your feet shoulder-width apart. Hold your arms at your sides and stand with your knees straight. Also, make sure to tighten your abs, keep your back flat, and squeeze your buttocks at the start of each burpee.\n\n2.  Bend at your hips and knees to lower yourself into a squatting position. Hinge forward with your hips and bend your knees slightly to bring your body down lower to the floor. Keep your back straight and your arms in close to your body as you do this.\n\n3. Place your hands on the floor in front of you. Keep your hands close together so they are directly under your shoulders when you place them on the ground. Hold your arms out straight, but don't lock out your elbows.\n\n4. Kick your feet back so that you are in the plank position. Jump both feet back behind you at the same time and position them about shoulder-width apart. Keep your hands planted firmly on the ground to support your body as you do this.\n\n5. Drop all the way down to the floor. Once you are in the plank position, carefully lower your chest all the way down to the floor to do a pushup. Keep your hands flat on the floor with your elbows up in the air and your arms close to your body.\n\n6. Press up with your arms and jump your feet forward. Keep your hands flat on the ground and push off from the ground with your hand as you hinge forward at the hips and jump your feet towards your chest. Do this in 1 smooth motion.\n\n7. Jump straight upwards to finish the move. Once your feet are back under your upper body, jump straight up with your hands and arms raised over your head and then land in the standing position. You can jump as high as you are able, or just do a little hop in the air to finish the burpee.",
-                        img: "burpees.gif"),
+                      title: "Burpees",
+                      hint: "(20 reps)",
+                      desc:
+                          "1. Stand with your feet shoulder-width apart. Hold your arms at your sides and stand with your knees straight. Also, make sure to tighten your abs, keep your back flat, and squeeze your buttocks at the start of each burpee.\n\n2.  Bend at your hips and knees to lower yourself into a squatting position. Hinge forward with your hips and bend your knees slightly to bring your body down lower to the floor. Keep your back straight and your arms in close to your body as you do this.\n\n3. Place your hands on the floor in front of you. Keep your hands close together so they are directly under your shoulders when you place them on the ground. Hold your arms out straight, but don't lock out your elbows.\n\n4. Kick your feet back so that you are in the plank position. Jump both feet back behind you at the same time and position them about shoulder-width apart. Keep your hands planted firmly on the ground to support your body as you do this.\n\n5. Drop all the way down to the floor. Once you are in the plank position, carefully lower your chest all the way down to the floor to do a pushup. Keep your hands flat on the floor with your elbows up in the air and your arms close to your body.\n\n6. Press up with your arms and jump your feet forward. Keep your hands flat on the ground and push off from the ground with your hand as you hinge forward at the hips and jump your feet towards your chest. Do this in 1 smooth motion.\n\n7. Jump straight upwards to finish the move. Once your feet are back under your upper body, jump straight up with your hands and arms raised over your head and then land in the standing position. You can jump as high as you are able, or just do a little hop in the air to finish the burpee.",
+                      img: "burpees.gif",
+                      index: 8,
+                    ),
                   ],
                 ),
               ),
